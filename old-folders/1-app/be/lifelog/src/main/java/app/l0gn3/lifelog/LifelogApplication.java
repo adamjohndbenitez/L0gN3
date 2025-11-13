2cf24184ece410a5070ceb3bf3fa8bb3339c57a0
AJ Benitez <aj@AJs-MacBook-Pro.local>
2025-11-13 13:09:59 -0500

    Folder Structure

---
+++ old-folders/1-app/be/lifelog/src/main/java/app/l0gn3/lifelog/LifelogApplication.java
@@ -0,0 +1,13 @@
+package app.l0gn3.lifelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifelogApplication.class, args);
+	}
+
+}
---
+++ old-folders/1-app/be/lifelog/src/test/java/app/l0gn3/lifelog/LifelogApplicationTests.java
@@ -0,0 +1,13 @@
+package app.l0gn3.lifelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
