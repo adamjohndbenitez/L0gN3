95f7862ac7341b199cfb17e61b0d6d4b40cd20d9
AJ Benitez <adamjohndbenitez@outlook.com>
2026-06-21 00:27:18 -0400

    Configure Confluent Cloud Schema Registry for joins exercise and Add Schema Registry config for Kafka Streams joins

--- B-skillset/B.0-sandbox/confluent-developer/learn-kafka-courses-main/kafka-streams/src/main/java/io/confluent/developer/joins/StreamsJoin.java
+++ B-skillset/B.0-sandbox/confluent-developer/learn-kafka-courses-main/kafka-streams/src/main/java/io/confluent/developer/joins/StreamsJoin.java
@@ -30,6 +30,7 @@ import java.util.concurrent.CountDownLatch;
 
 public class StreamsJoin {
 
+    // Use a static helper method to get SerDes for your Avro records (in subsequent exercises, you'll abstract this into a static utility method, in the StreamsUtils class of the course repo):
     static <T extends SpecificRecord> SpecificAvroSerde<T> getSpecificAvroSerde(final Map<String, Object> serdeConfig) {
         final SpecificAvroSerde<T> specificAvroSerde = new SpecificAvroSerde<>();
         specificAvroSerde.configure(serdeConfig, false);
@@ -37,22 +38,29 @@ public class StreamsJoin {
     }
 
     public static void main(String[] args) throws IOException {
+
+        // Use a utility method to load the properties (you can refer to the StreamsUtils class
         Properties streamsProps = StreamsUtils.loadProperties();
         streamsProps.put(StreamsConfig.APPLICATION_ID_CONFIG, "joining-streams");
 
         StreamsBuilder builder = new StreamsBuilder();
+
+        // Get the input topic names and the output topic name from the properties (look at streams.properties (gitignored)):
         String streamOneInput = streamsProps.getProperty("stream_one.input.topic");
         String streamTwoInput = streamsProps.getProperty("stream_two.input.topic");
         String tableInput = streamsProps.getProperty("table.input.topic");
         String outputTopic = streamsProps.getProperty("joins.output.topic");
 
+        // Create a HashMap of the configurations:
         Map<String, Object> configMap = StreamsUtils.propertiesToMap(streamsProps);
 
+        // Then create the required SerDes for all streams and for the table:
         SpecificAvroSerde<ApplianceOrder> applianceSerde = getSpecificAvroSerde(configMap);
         SpecificAvroSerde<ElectronicOrder> electronicSerde = getSpecificAvroSerde(configMap);
         SpecificAvroSerde<CombinedOrder> combinedSerde = getSpecificAvroSerde(configMap);
         SpecificAvroSerde<User> userSerde = getSpecificAvroSerde(configMap);
 
+        // Create the ValueJoiner for the stream-table join:
         ValueJoiner<ApplianceOrder, ElectronicOrder, CombinedOrder> orderJoiner =
                 (applianceOrder, electronicOrder) -> CombinedOrder.newBuilder()
                         .setApplianceOrderId(applianceOrder.getOrderId())
@@ -60,6 +68,7 @@ public class StreamsJoin {
                         .setElectronicOrderId(electronicOrder.getOrderId())
                         .setTime(Instant.now().toEpochMilli())
                         .build();
+        // The stream is a result of the preceding stream-stream join, but it's a left outer join, because the right-side record might not exist.
 
         ValueJoiner<CombinedOrder, User, CombinedOrder> enrichmentJoiner = (combined, user) -> {
             if (user != null) {
@@ -68,6 +77,7 @@ public class StreamsJoin {
             return combined;
         };
 
+        // Create the ApplianceOrder stream as well as the ElectronicOrder stream:
         KStream<String, ApplianceOrder> applianceStream =
                 builder.stream(streamOneInput, Consumed.with(Serdes.String(), applianceSerde))
                         .peek((key, value) -> System.out.println("Appliance stream incoming record key " + key + " value " + value));
@@ -76,31 +86,42 @@ public class StreamsJoin {
                 builder.stream(streamTwoInput, Consumed.with(Serdes.String(), electronicSerde))
                         .peek((key, value) -> System.out.println("Electronic stream incoming record " + key + " value " + value));
 
+        // From here, create the User table:
         KTable<String, User> userTable =
                 builder.table(tableInput, Materialized.with(Serdes.String(), userSerde));
 
-        KStream<String, CombinedOrder> combinedStream = null;
+        KStream<String, CombinedOrder> combinedStream = applianceStream.join(
         // create a Join between the applianceStream and the electronicStream
         // using the ValueJoiner created above, orderJoiner gets you the correct value type of CombinedOrder
         // You want to join records within 30 minutes of each other HINT: JoinWindows and Duration.ofMinutes
         // Add the correct Serdes for the join state stores remember both sides have same key type
         // HINT: StreamJoined and Serdes.String  and Serdes for the applianceStream and electronicStream created above
+                electronicStream,
+                orderJoiner,
+                JoinWindows.of(Duration.ofMinutes(30)),
+                StreamJoined.with(Serdes.String(), applianceSerde, electronicSerde)
+        )
 
         // Optionally add this statement after the join to see the results on the console
-        // .peek((key, value) -> System.out.println("Stream-Stream Join record key " + key + " value " + value));
-
+        // Add a peek operator to view the results of the join:
+         .peek((key, value) -> System.out.println("Stream-Stream Join record key " + key + " value " + value));
 
         // Now join the combinedStream with the userTable,
         // but you'll always want a result even if no corresponding entry is found in the table
         // Using the ValueJoiner created above, enrichmentJoiner, return a CombinedOrder instance enriched with user information
         // You'll need to add a Joined instance with the correct Serdes for the join state store
+        // Call the join method on the KStream that results from the join in previous steps, adding the userTable as the right side in the stream-table join. Then add enrichmentJoiner to add user information, if available. Add the Joined object with SerDes for the values of both sides of the join, add the peek operator to view the stream-table join results, and write the final join results to a topic:
+        combinedStream.leftJoin(
+                        userTable,
+                        enrichmentJoiner,
+                        Joined.with(Serdes.String(), combinedSerde, userSerde))
 
         // Add these two statements after the join call to print results to the console and write results out
         // to a topic
+         .peek((key, value) -> System.out.println("Stream-Table Join record key " + key + " value " + value))
+         .to(outputTopic, Produced.with(Serdes.String(), combinedSerde));
 
-        // .peek((key, value) -> System.out.println("Stream-Table Join record key " + key + " value " + value))
-        // .to(outputTopic, Produced.with(Serdes.String(), combinedSerde));
-
+        // Create the KafkaStreams object, and again use the TopicLoader helper class to create topics and produce exercise data:
         try (KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), streamsProps)) {
             final CountDownLatch shutdownLatch = new CountDownLatch(1);
 
@@ -110,6 +131,7 @@ public class StreamsJoin {
             }));
             TopicLoader.runProducer();
             try {
+                // Finally, start the Kafka Streams application:
                 kafkaStreams.start();
                 shutdownLatch.await();
             } catch (Throwable e) {
--- B-skillset/B.0-sandbox/confluent-developer/learn-kafka-courses-main/kafka-streams/src/main/java/io/confluent/developer/ktable/KTableExample.java
+++ B-skillset/B.0-sandbox/confluent-developer/learn-kafka-courses-main/kafka-streams/src/main/java/io/confluent/developer/ktable/KTableExample.java
@@ -29,23 +29,36 @@ public class KTableExample {
         final String inputTopic = streamsProps.getProperty("ktable.input.topic");
         final String outputTopic = streamsProps.getProperty("ktable.output.topic");
 
+        // Start by creating a variable to store the string that we want to filter on:
         final String orderNumberStart = "orderNumber-";
 
         // Crate a table with the StreamBuilder from above and use the table method
         // along with the inputTopic create a Materialized instance and name the store
         // and provide a Serdes for the key and the value  HINT: Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as
         // then use two methods to specify the key and value serde
-        KTable<String, String> firstKTable = null;
+        KTable<String, String> firstKTable = builder.table(
+                inputTopic,
+                Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as("ktable-store")
+                    // Add SerDes for the key and value on your Materialized object:
+                    .withKeySerde(Serdes.String())
+                    .withValueSerde(Serdes.String())
+        );
 
+        // Add a filter operator for removing records that don't contain the order number variable value:
         firstKTable.filter((key, value) -> value.contains(orderNumberStart))
+                // Map the values by taking a substring:
                 .mapValues(value -> value.substring(value.indexOf("-") + 1))
-                .filter((key, value) -> Long.parseLong(value) > 1000);
+                // Then filter again by taking out records where the number value of the string is less than or equal to 1000:
+                .filter((key, value) -> Long.parseLong(value) > 1000)
                 // Add a method here to covert the table to a stream
+                .toStream()
                 // Then uncomment the following two lines to view results on the console and write to a topic
-                //.peek((key, value) -> System.out.println("Outgoing record - key " +key +" value " + value))
-                //.to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
+                // Add a peek operation to view the key values from the table:
+                .peek((key, value) -> System.out.println("Outgoing record - key " +key +" value " + value))
+                // Write the records to a topic:
+                .to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
 
-
+        // Create a KafkaStreams object and run the topic data helper utility:
         try (KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), streamsProps)) {
             final CountDownLatch shutdownLatch = new CountDownLatch(1);
 
@@ -55,6 +68,7 @@ public class KTableExample {
             }));
             TopicLoader.runProducer();
             try {
+                // kafkaStreams.start();
                 kafkaStreams.start();
                 shutdownLatch.await();
             } catch (Throwable e) {
